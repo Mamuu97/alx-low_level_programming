62cd2da3419ce71838e623d33a3f74f6cc15af35
Mamuu97 <maryumajumaan@gmail.com>
2022-07-05 15:28:02 -0700

    prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+1#include "main.h";
+2
+3/**main - prints putchar
+4* Return: always 0
+5*/
+6int main(void)
+7{
+8     _putchar('_');
+9     _putchar('p');
+10    _putchar('u');
+11    _putchar('t');
+12    _putchar('c');
+13    _putchar('h');
+14    _putchar('a');
+15    _putchar('r');
+16    _putchar('\n');
+17
+18    return (0);
+19}
